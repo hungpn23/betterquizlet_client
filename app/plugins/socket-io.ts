e2b5f2f16f36057145c1011554adbf35fb7cddb5
Sotatek-HungPham7 <hung.pham7@sotatek.com>
2026-01-30 13:38:01 +0700

    refactor: conditionally initialize socket io client

--- app/plugins/socket-io.ts
+++ app/plugins/socket-io.ts
@@ -15,24 +15,26 @@ declare module "vue" {
 export default defineNuxtPlugin(() => {
 	const { token } = useAuth();
 
-	const socket = io("http://localhost:3001/notifications", {
-		extraHeaders: { Authorization: token.value || "" },
-		withCredentials: true,
-	});
-
-	socket.on("connect", () => {
-		socket.on("socketConnected", (_message: string) => {
-			console.log("Connected to Socket.IO server.");
+	if (token.value) {
+		const socket = io("http://localhost:3001/notifications", {
+			extraHeaders: { Authorization: token.value || "" },
+			withCredentials: true,
 		});
-	});
 
-	socket.on("disconnect", () => {
-		console.log("Disconnected from Socket.IO server.");
-	});
+		socket.on("connect", () => {
+			socket.on("socketConnected", (_message: string) => {
+				console.log("Connected to Socket.IO server.");
+			});
+		});
 
-	socket.on("connect_error", (_error: Error) => {
-		console.error("Socket.IO connection error.");
-	});
+		socket.on("disconnect", () => {
+			console.log("Disconnected from Socket.IO server.");
+		});
 
-	return { provide: { socket } };
+		socket.on("connect_error", (_error: Error) => {
+			console.error("Socket.IO connection error.");
+		});
+
+		return { provide: { socket } };
+	}
 });
